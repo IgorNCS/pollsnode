ab875bdcd9123ba25358906bc28b27a6596e741b
Igor Nascimento <64155074+IgorNCS@users.noreply.github.com>
2022-04-12 09:06:43 -0300

    chore: add jest again

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+    test('Should return 400 if no name isi provided',() => {
+        const sut = new SignUpController()
+
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                passwordConfimation: 'any_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+})
